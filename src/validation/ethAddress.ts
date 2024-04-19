58b2670182c4d1a0c6ca63f065933ad5ffb55f59
Travis Bloom <travisbloom@gmail.com>
2024-04-19 19:39:34 -0400

    tweaks

--- src/validation/ethAddress.ts
+++ src/validation/ethAddress.ts
@@ -3,5 +3,8 @@ import { string } from 'zod'
 import { normalizeETHAddress } from '@/utils/shared/normalizedETHAddress'
 
 export const zodEthereumAddress = string()
-  .regex(/^0x[a-fA-F0-9]{40}$/, 'Must be an ethereum address (starts with "0x") or a valid ENS')
+  .regex(
+    /^0x[a-fA-F0-9]{40}$/,
+    'Must be an ethereum address (starts with "0x"). We\'re working to add support for ENS soon!',
+  )
   .transform(str => normalizeETHAddress(str))
