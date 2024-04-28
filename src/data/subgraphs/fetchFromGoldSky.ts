8db7cc78d3e41e96aaf2a44534d0124e8f2731a4
Travis Bloom <travisbloom@gmail.com>
2024-04-27 21:30:00 -0400

    retry goldsky

--- src/data/subgraphs/fetchFromGoldSky.ts
+++ src/data/subgraphs/fetchFromGoldSky.ts
@@ -1,5 +1,7 @@
 import 'server-only'
 
+import pRetry from 'p-retry'
+
 import { fetchReq } from '@/utils/shared/fetchReq'
 import { getLogger } from '@/utils/shared/logger'
 import { requiredEnv } from '@/utils/shared/requiredEnv'
@@ -15,17 +17,36 @@ const API_ENDPOINT =
     : 'https://api.goldsky.com/api/private/project_clv5n6dbek7v101zj51lr5n21/subgraphs/getreccd-testing-base-sepolia/2/gn'
 export const fetchFromGoldSky = async <R, V = object>(query: string, variables?: V) => {
   logger.debug(`fetchFromGoldSky called`)
-  const response = await fetchReq(API_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${GOLDSKY_API_KEY}`,
+  function attempt() {
+    const controller = new AbortController()
+    const timeoutId = setTimeout(() => controller.abort(), 5000)
+    return fetchReq(API_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${GOLDSKY_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    }).then(res => {
+      clearTimeout(timeoutId)
+      return res
+    })
+  }
+  const response = await pRetry(attempt, {
+    onFailedAttempt: error => {
+      console.log(
+        `Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`,
+      )
+      // 1st request => Attempt 1 failed. There are 4 retries left.
+      // 2nd request => Attempt 2 failed. There are 3 retries left.
+      // …
     },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
+    retries: 3,
   })
+
   logger.debug(`fetchFromGoldSky returned with status ${response.status}`)
   const json = (await response.json()) as { data: R } | { errors: any[] }
   if ('errors' in json) {
